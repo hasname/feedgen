79f7b670348ad1507040a26e86ef42fe206a31bb
Gea-Suan Lin <gslin@gslin.org>
2018-09-16 22:16:10 +0800

    Fix path.

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo su - www-data -c "pip install -U -r /srv/feedgen.hasname.com/requirements.txt"
+~/.pyenv/shims/pip install -U -r /srv/feedgen.hasname.com/requirements.txt
 pkill -INT uwsgi
 LANG=en_US.UTF-8 ~/.pyenv/shims/uwsgi --ini /srv/feedgen.hasname.com/uwsgi.ini > /dev/null 2>&1 &
