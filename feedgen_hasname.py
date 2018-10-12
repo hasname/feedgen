07e4f3b2482043826013c2a28ea589b53ed76f85
Gea-Suan Lin <gslin@gslin.org>
2018-10-12 21:40:20 +0800

    Implement 1111 feed.

--- feedgen_hasname.py
+++ feedgen_hasname.py
@@ -27,6 +27,44 @@ def robotstxt():
     bottle.response.set_header('Content-Type', 'text/plain')
     return '#\nUser-agent: *\nDisallow: /\n'
 
+@app.route('/1111/<keyword>')
+def job1111(keyword):
+    url = 'https://www.1111.com.tw/job-bank/job-index.asp?flag=13&ks={}&fs=1&si=1&ts=4&col=da&sort=desc'.format(urllib.parse.quote_plus(keyword))
+
+    title = '1111 搜尋 - {}'.format(keyword)
+
+    feed = feedgen.feed.FeedGenerator()
+    feed.author({'name': 'Feed Generator'})
+    feed.id(url)
+    feed.link(href=url, rel='alternate')
+    feed.title(title)
+
+    r = requests.get(url, headers={'User-agent': user_agent}, timeout=5)
+    body = lxml.html.fromstring(r.text)
+
+    for item in body.cssselect('li.digest'):
+        a = item.cssselect('a.mobiFullLInk')[0]
+
+        job_title = a.get('title')
+
+        job_url = a.get('href')
+        if job_url.startswith('//'):
+            job_url = 'https:' + job_url
+
+        job_desc = item.cssselect('.jbInfoTxt')[0].text_content()
+        content = '{}'.format(html.escape(job_desc))
+
+        entry = feed.add_entry()
+        entry.content(content, type='xhtml')
+        entry.id(job_url)
+        entry.link(href=job_url)
+        entry.title(job_title)
+
+    bottle.response.set_header('Cache-Control', 'max-age=300,public')
+    bottle.response.set_header('Content-Type', 'application/atom+xml')
+
+    return feed.atom_str()
+
 @app.route('/pchome/<keyword>')
 def pchome(keyword):
     url = 'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q={}&page=1&sort=new/dc'.format(urllib.parse.quote_plus(keyword))
