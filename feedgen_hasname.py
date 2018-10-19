dd187aad7d781c8d38ffda2483681b148895d1e5
Gea-Suan Lin <gslin@gslin.org>
2018-10-19 22:32:01 +0800

    Implement 518 feed.

--- feedgen_hasname.py
+++ feedgen_hasname.py
@@ -110,6 +110,42 @@ def job1111(keyword):
 
     return feed.atom_str()
 
+@app.route('/518/<keyword>')
+def job518(keyword):
+    url = 'https://www.518.com.tw/job-index-P-1.html?i=1&am=1&ad={}&orderType=1&orderField=8'.format(urllib.parse.quote_plus(keyword))
+
+    title = '518 搜尋 - {}'.format(keyword)
+
+    feed = feedgen.feed.FeedGenerator()
+    feed.author({'name': 'Feed Generator'})
+    feed.id(url)
+    feed.link(href=url, rel='alternate')
+    feed.title(title)
+
+    r = requests.get(url, headers={'User-agent': user_agent}, timeout=5)
+    body = lxml.html.fromstring(r.text)
+
+    for item in body.cssselect('#listContent > ul'):
+        a = item.cssselect('li.title a')[0]
+        job_title = a.text_content()
+        job_url = a.get('href')
+
+        job_company = item.cssselect('li.company')[0].text_content()
+
+        job_desc = item.cssselect('li.sumbox')[0].text_content()
+        content = '<h3>{}</h3><p>{}</p>'.format(html.escape(job_company), html.escape(job_desc))
+
+        entry = feed.add_entry()
+        entry.content(content, type='xhtml')
+        entry.id(job_url)
+        entry.link(href=job_url)
+        entry.title(job_title)
+
+    bottle.response.set_header('Cache-Control', 'max-age=300,public')
+    bottle.response.set_header('Content-Type', 'application/atom+xml')
+
+    return feed.atom_str()
+
 @app.route('/pchome/<keyword>')
 def pchome(keyword):
     url = 'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q={}&page=1&sort=new/dc'.format(urllib.parse.quote_plus(keyword))
