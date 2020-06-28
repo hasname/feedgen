bbe82b727905a5691f22725f3429637c05652c23
Gea-Suan Lin <gslin@gslin.org>
2020-06-29 03:53:03 +0800

    Fix route.

--- general/urls.py
+++ general/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import job104, robotstxt
+from .views import job104, job1111, robotstxt
 
 urlpatterns = [
     path('robots.txt', robotstxt.RobotsTxtView.as_view(), name='robotstxt'),
 
     path('104/<str:keyword>', job104.Job104View.as_view(), name='job104'),
-    path('1111/<str:keyword>', job104.Job104View.as_view(), name='job1111'),
+    path('1111/<str:keyword>', job1111.Job1111View.as_view(), name='job1111'),
 ]
