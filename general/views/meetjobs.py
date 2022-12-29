1a298af73cff27cdf49d3b5b9e73cfed453f2312
Gea-Suan Lin <gslin@gslin.org>
2022-12-30 01:19:35 +0800

    Capture TypeError.

--- general/views/meetjobs.py
+++ general/views/meetjobs.py
@@ -33,27 +33,31 @@ class MeetJobsView(View):
             items = []
 
         for item in items:
-            job_company = item['employer']['name']
-            job_desc = item['description']
-            job_features = item['work_type']
-            job_link = 'https://meet.jobs/zh-TW/jobs/{}-{}'.format(item['id'], item['slug'])
-            job_published_at = item['published_at']
-            job_title = item['title']
-            job_updated_at = item['updated_at']
+            try:
+                job_company = item['employer']['name']
+                job_desc = item['description']
+                job_features = item['work_type']
+                job_link = 'https://meet.jobs/zh-TW/jobs/{}-{}'.format(item['id'], item['slug'])
+                job_published_at = item['published_at']
+                job_title = item['title']
+                job_updated_at = item['updated_at']
 
-            item_author = job_company
-            item_content = '<p>{}</p><p>{}</p>'.format(html.escape(job_features), html.escape(job_desc))
-            item_title = job_title
-            item_url = job_link
+                item_author = job_company
+                item_content = '<p>{}</p><p>{}</p>'.format(html.escape(job_features), html.escape(job_desc))
+                item_title = job_title
+                item_url = job_link
 
-            entry = feed.add_entry()
-            entry.author({'name': item_author})
-            entry.content(item_content, type='xhtml')
-            entry.id(item_url)
-            entry.link(href=item_url)
-            entry.published(datetime.datetime.strptime(item['published_at'], '%Y-%m-%dT%H:%M:%S.%f%z'))
-            entry.title(item_title)
-            entry.updated(datetime.datetime.strptime(item['updated_at'], '%Y-%m-%dT%H:%M:%S.%f%z'))
+                entry = feed.add_entry()
+                entry.author({'name': item_author})
+                entry.content(item_content, type='xhtml')
+                entry.id(item_url)
+                entry.link(href=item_url)
+                entry.published(datetime.datetime.strptime(item['published_at'], '%Y-%m-%dT%H:%M:%S.%f%z'))
+                entry.title(item_title)
+                entry.updated(datetime.datetime.strptime(item['updated_at'], '%Y-%m-%dT%H:%M:%S.%f%z'))
+
+            except TypeError:
+                pass
 
         res = HttpResponse(feed.atom_str(), content_type='application/atom+xml; charset=utf-8')
         res['Cache-Control'] = 'max-age=300,public'
